b49133f228690ea9d3bab803edfc709d14bc0565
Jeff McGee <JeffAMcGee@gmail.com>
2013-01-25 15:25:14 -0600

    fix zoom on map

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var map = po.map()
     .add(po.interact())
     .add(po.compass().pan("none"));
 */
-var map = L.map('map').setView([38, -98], 5);
+var map = L.map('map',{maxZoom:13,minZoom:2}).setView([38, -98], 5);
 
 L.tileLayer('http://{s}.tile.cloudmade.com/3c84c4f923824f7cb6836564e90876f3/84377/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://cloudmade.com">CloudMade</a>',
